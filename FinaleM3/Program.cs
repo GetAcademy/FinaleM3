e70a67bc3ae7da8b54bce379f817ef4831a1c0b2
GET Academy <terje@getacademy.no>
2023-05-10 13:50:46 +0200

    Initial commit

---
+++ FinaleM3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * To hovedråd på vei ut av M3:
+ *  - Objektorientert programmering
+ *  - Full stack
+ *
+ * Objektorientert programmering
+ *  - GrepS
+ *  - Be om tilbakemelding på kode - og dere i mellom
+ *  - Innkapsling!
+ *  - Modellering
+ *
+ * Full stack
+ *  - Frontend:
+ *      - Patterns: MVC, MVP, MVVM, MVU)
+ *      - Rammeverk (React, Angular, Vue) - inkl. static rendering og state management
+ *  - Backend
+ *      - Dependency Inversion, Lagdeling, Onion arkitektur
+ *        Domain driven design, microservices, CQRS, Event sourcing
+ *        Domain events
+ *  - Database
+ *      - SQL
+ *          - design
+ *          - ytelse, indekser
+ *          - avanserte spørringer
+ *      - ORMer som EntityFramework
+ *      - NOSQL-databaser som MongoDb
+ *
+ * MEN - GET Prepared + vente på bedrift kan være tung tid
+ *  - Følg energien - gjør noe som er gøy!
+ *  - Øve nøkkelkompetanser
+ *      - Selvdisiplin
+ *      - Kommunikasjon og samarbeid
+ *      - Vaner
+ *      - Motivasjon
+ *      - Locus of control
+ *      - Growth mindset
+ *
+ * Litt til inspirasjon:
+ *  - Rekursjon
+ *  - Datastrukturer
+ *  - Rekursive datastrukturer
+ *  - Funksjonell programmering og F#
+ *
+ */
